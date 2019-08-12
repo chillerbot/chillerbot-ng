6fd7b3ead85f582ca69b254aa2dbed13bfd8007f
ChillerDragon <chillerdragon@gmail.com>
2019-08-12 12:26:31 +0200

    Add version info in chat

--- src/game/client/components/chat.cpp
+++ src/game/client/components/chat.cpp
@@ -14,6 +14,7 @@
 
 #include <game/client/components/scoreboard.h>
 #include <game/localization.h>
+#include <game/version.h>
 
 #include "chat.h"
 
@@ -139,12 +140,32 @@ void CChat::EnableMode(int Team)
   return;
 }
 
+void CChat::ChillerbotMessage(const char *pMsg)
+{
+	if(str_find_nocase(pMsg, "bot?") ||
+		str_find_nocase(pMsg, "info();") ||
+		str_find_nocase(pMsg, "version();") ||
+		str_find_nocase(pMsg, "help();") ||
+		str_find_nocase(pMsg, "!help") ||
+		str_find_nocase(pMsg, "!info"))
+	{
+		char aBuf[128];
+		str_format(aBuf, sizeof(aBuf), "[chillerbot-ng] chillerbot.png headless (v%s)", CHILLERBOT_VERSION);
+		Say(0, aBuf);
+	}
+	else if(str_find_nocase(pMsg, "I was a faithful friend from the start. The best friend? No. But I was a faithful friend. From map to map, I helped jao get across hard barriers. We talked and talked, about the future, what teeworlds could hold. We had wonderful moments together, ..."))
+	{
+		Say(0, "... memories that would last a lifetime. Whenever I would see him on a server, a new journey would spark up as we explored the wonders of each map. And now? he won't fucking transfer my points");
+	}
+}
+
 void CChat::OnMessage(int MsgType, void *pRawMsg)
 {
 	if(MsgType == NETMSGTYPE_SV_CHAT)
 	{
 		CNetMsg_Sv_Chat *pMsg = (CNetMsg_Sv_Chat *)pRawMsg;
 		AddLine(pMsg->m_ClientID, pMsg->m_Team, pMsg->m_pMessage);
+		ChillerbotMessage(pMsg->m_pMessage);
 	}
 }
 
--- src/game/version.h
+++ src/game/version.h
@@ -6,5 +6,6 @@
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 #define GAME_RELEASE_VERSION "11.8"
 #define CLIENT_VERSIONNR 11080
+#define CHILLERBOT_VERSION "0001"
 extern const char *GIT_SHORTREV_HASH;
 #endif
